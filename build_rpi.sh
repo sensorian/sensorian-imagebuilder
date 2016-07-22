b6147447f1228b60c06cabbe8a9682594633f4d6
Dylan Kauling <gunsmithy@gmail.com>
2016-07-21 21:44:28 -0400

    Significantly changed the build script and included code
    Build script no longer contains gateway/nameserver/proxy settings for
    simplicity. Advanced users would be able to configure this on their own.
    Updated the BCM2835 library used to version 1.50 from 1.44 and fixed the
    hash. Updated dependency installation to coincide with a Raspbian Jessie
    image rather than Wheezy. Removed minicom as it is not a requirement,
    apache and sql may not be required either. Changed node-red install to
    work with Jessie. Made the script clone the firmware repo instead of
    extracting a zip and deleted said zip. Deleted all the included Python
    SO code and i2c C code to remove duplicates across repositories. This
    can all be found in the firmware repo which gets cloned by the script.
    Changed compilation steps to match the new code locations in the cloned
    directory. Added the Sensorian Interface and Sensorian Hub Client to be
    cloned, dependency installed and compiled as well. Need to try to update
    dependency versions if possible and remove PIL in favour of Pillow as
    even the included firmware example programs memory leak when drawing to
    the TFT LCD.

--- build_rpi.sh
+++ build_rpi.sh
@@ -1,25 +1,12 @@
-#User parameters to modify based on your configuration
-USER_DEFAULT_GW="192.168.137.1"
-USER_NAMESERVER="10.120.200.63"
-USER_APT_SERVER="http://mirrordirector.raspbian.org/raspbian/"
-
 #Constants
-checksum_sha512_bcm2835="d1f006614cb1f95e1a6c33081e3a932984d54b00f535d6b7ccde774ff59ac40a858c5f7822e6f5103d207cacf8ee0a8325c2d36673607b54b4fdcdb090209c25"
-py_spidev_commit="4f3f1dea5d4828c387e8822aa6bf3920a4119c64"
-
-# Configure default gateway and nameserver - Change to suit your network
-sudo route add default gw "$USER_DEFAULT_GW"
-sudo sh -c 'echo "nameserver 10.120.200.63" > /etc/resolv.conf'
-
-#Configure apt-caching proxy - Remove this line if you have none.
-sudo sh -c 'echo "Acquire::http::Proxy \"http://192.168.137.3:3128\";" > /etc/apt/apt.conf'
+checksum_sha512_bcm2835="94887f04616c3bea4e2cb5d265068208958777feffb88ccbb33a03e9d8b8a7f06f06d9744dae45cd33d24f17a29ae1fff437a23521084667a57de8208855105c"
 
 #Download BCM2835 library
 echo "Downloading bcm2835"
-wget www.airspayce.com/mikem/bcm2835/bcm2835-1.44.tar.gz -O bcm2835-1.44.tar.gz || { echo "Failed to download bcm2835 library" && exit; }
+wget http://www.airspayce.com/mikem/bcm2835/bcm2835-1.50.tar.gz -O bcm2835-1.50.tar.gz || { echo "Failed to download bcm2835 library" && exit; }
 
 #Verify the SHA512 Hash
-bcm2835_sha512=$(sha512sum bcm2835-1.44.tar.gz | head -c 128)
+bcm2835_sha512=$(sha512sum bcm2835-1.50.tar.gz | head -c 128)
 
 if [ "$bcm2835_sha512"=="$checksum_sha512_bcm2835" ]
 	then
@@ -30,12 +17,12 @@ if [ "$bcm2835_sha512"=="$checksum_sha512_bcm2835" ]
 		exit
 fi
 
-echo "Extracting the downloaded file"
-tar -zxvf bcm2835-1.44.tar.gz
+echo "Extracting the BCM2835 library"
+tar -zxf bcm2835-1.50.tar.gz
 
 #Compile
 echo "Building BCM2835 library"
-cd bcm2835-1.44
+cd bcm2835-1.50
 ./configure
 sudo make check
 sudo make install
@@ -47,44 +34,12 @@ sudo cp -p src/libbcm2835.so /usr/lib/
 #Switch to parent directory
 cd ..
 
-#Setup apt sources with only wheezy repo, for now.
-#mycmd='echo "deb $USER_APT_SERVER wheezy main contrib non-free rpi" > /etc/apt/sources.list'
-sudo sh -c 'echo "deb http://mirrordirector.raspbian.org/raspbian/ wheezy main contrib non-free rpi" > /etc/apt/sources.list'
 sudo apt-get update || { echo "Failed to apt-get update" && exit; }
-
-#Install git
-sudo apt-get -y install git || { echo "Failed to apt-get install git" && exit; }
 
 #Install python-dev
 sudo apt-get -y install python-dev || { echo "Failed to install python-dev" && exit; }
 
-#Clone the py-spidev repository
-#git clone https://github.com/doceme/py-spidev || { echo "Failed to clone py-spidev git repo" && exit; }
-
-#cd py-spidev/
-
-##Switch to a commit known to work
-#git checkout "$py_spidev_commit"
-
-##Verify the repository
-#head_commit=$(git rev-parse --verify HEAD)
-
-#if [ "$head_commit"=="$py_spidev_commit" ]
-	#then
-		#echo "Commit checksum verification PASSED"
-	#else
-		#echo "Commit checksum verification FAILED. Something is wrong with the py-spidev repository."
-		#echo "Exiting..."
-		#exit
-#fi
-
-##Install py-spidev
-#sudo python setup.py install
-
-#cd ..
-
 #Install other packages from the Raspbian Repository
-sudo apt-get -y install minicom || { echo "Failed to install minicom" && exit; }
 sudo apt-get -y install i2c-tools || { echo "Failed to install i2c-tools" && exit; }
 sudo apt-get -y install libi2c-dev || { echo "Failed to install libi2c-dev" && exit; }
 sudo apt-get -y install python-smbus || { echo  "Failed to install python-smbus" && exit; }
@@ -144,38 +99,20 @@ head /dev/zero -c 2 > ~/.sensorian/mmap_ambientlight
 head /dev/zero -c 2 > ~/.sensorian/mmap_pressure
 head /dev/zero -c 2 > ~/.sensorian/mmap_temperature
 
-#nodejs and npm need to be installed from the Raspbian Jessie repo. So let's setup apt-pinning.
-sudo sh -c 'echo "deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi" >> /etc/apt/sources.list'
-sudo sh -c 'echo "Package: *" > /etc/apt/preferences'
-sudo sh -c 'echo "Pin: release a=wheezy" >> /etc/apt/preferences'
-sudo sh -c 'echo "Pin-Priority: 700" >> /etc/apt/preferences'
-sudo sh -c 'echo "Package: *" >> /etc/apt/preferences'
-sudo sh -c 'echo "Pin: release a=jessie" >> /etc/apt/preferences'
-sudo sh -c 'echo "Pin-Priority: 650" >> /etc/apt/preferences'
-sudo apt-get update || { echo "Wheezy and Jessie apt-get update failed" && exit; }
-
-#Download and install Node.js for Raspberry Pi from Jessie repo
-sudo apt-get -y -t jessie install nodejs || { echo "Failed to install nodejs" && exit; }
-
-#soft-link /usr/bin/node to /usr/bin/nodejs
-sudo ln -s -T /usr/bin/nodejs /usr/bin/node
-
-#Install Node Package Manager (NPM)
-sudo apt-get -y -t jessie install npm || { echo "Failed to install npm" && exit; }
-
-#Download and install Node-RED
-sudo npm install -g --unsafe-perm node-red || { echo "Failed to install node-RED" && exit; }
-
 #Install Sensorian plugins into Node-RED nodes directory
-sudo cp -p -r nodered-plugins/* /usr/local/lib/node_modules/node-red/nodes/
+mkdir -p ~/.node-red/nodes
+sudo cp -p -r nodered-plugins/* /usr/lib/node_modules/node-red/nodes
 
-#Create and populate ~/Sensorian/
-tar -xvf sensorian_directory.tar.gz -C ~
+#Get a reference to the current directory
+builddir=$(pwd)
+
+#Clone the latest version of the Sensorian Firmware repository Fixes branch into Sensorian
+git clone https://github.com/sensorian/sensorian-firmware.git -b Fixes --single-branch ~/Sensorian
 chmod +x ~/Sensorian/Handler_NodeRED/run_servers.sh
 
 #Compile all needed .so files
 #Enter PythonSharedObjectSrc
-cd PythonSharedObjectSrc
+cd ~/Sensorian/Drivers_Python/PythonSharedObjectSrc
 
 cd MPL3115A2
 make
@@ -189,44 +126,88 @@ cd CAP1203
 make
 cd ..
 
-#Leave PythonSharedObjectSrc
-cd ..
-
 #Enter i2c-devices-interface
-cd i2c-devices-interface
-
+cd ~/Sensorian/Handler_Scratch/utilities/i2c-devices-interface
 make
 
-#Leave i2c-devices-interface
-cd ..
+#Return to Sensorian directory
+cd ~/Sensorian
 
 #Copy the files into where they are needed
-cp -p PythonSharedObjectSrc/MPL3115A2/libMPL.so ~/Sensorian/Drivers_Python/MPL3115A2/libMPL.so
-cp -p PythonSharedObjectSrc/MPL3115A2/libMPL.so ~/Sensorian/Apps_Python/P03-Barometer/libMPL.so
-cp -p PythonSharedObjectSrc/MPL3115A2/libMPL.so ~/Sensorian/Apps_Python/P06-FlaskSensors/Dashboard/libMPL.so
+cp -p Drivers_Python/PythonSharedObjectSrc/MPL3115A2/libMPL.so Apps_Python/P03-Barometer/libMPL.so
+cp -p Drivers_Python/PythonSharedObjectSrc/MPL3115A2/libMPL.so Apps_Python/P06-FlaskSensors/dashboard/libMPL.so
 
-cp -p PythonSharedObjectSrc/FXOS8700CQR1/libFXO.so ~/Sensorian/Drivers_Python/FXOS8700CQR1/libFXO.so
-cp -p PythonSharedObjectSrc/FXOS8700CQR1/libFXO.so ~/Sensorian/Apps_Python/P07-Dynamic\ Photo\ Frame/libFXO.so
+cp -p PythonSharedObjectSrc/FXOS8700CQR1/libFXO.so Apps_Python/P07-Dynamic\ Photo\ Frame/libFXO.so
 
-cp -p PythonSharedObjectSrc/CAP1203/libCAP.so ~/Sensorian/Drivers_Python/CAP1203/libCAP.so
-cp -p PythonSharedObjectSrc/CAP1203/libCAP.so ~/Sensorian/Apps_Python/P04-CapacitiveTouch/libCAP.so
+cp -p PythonSharedObjectSrc/CAP1203/libCAP.so Apps_Python/P04-CapacitiveTouch/libCAP.so
 
-cp -p i2c-devices-interface/libsensorian.so ~/Sensorian/Handler_Scratch/libsensorian.so
-cp -p i2c-devices-interface/libsensorian.so ~/Sensorian/Handler_NodeRED/libsensorian.so
-
-#Scratch is already configured in Raspbian-Wheezy-2015-05-05 with MESH enabled so nothing to do here.
+cp -p Handler_Scratch/utilities/i2c-devices-interface/libsensorian.so Handler_Scratch/libsensorian.so
+cp -p Handler_Scratch/utilities/i2c-devices-interface/libsensorian.so Handler_NodeRED/libsensorian.so
 
 #Enable SPI and I2C interfaces
 sudo sed -i 's/#dtparam=i2c_arm=on/dtparam=i2c_arm=on/g' /boot/config.txt
 sudo sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
 sudo sh -c 'echo i2c-dev >> /etc/modules'
 
-#Get a reference to the current directory
-builddir=$(pwd)
-
 #Build the IP demo
-cd ~/Sensorian/Apps_C/P05-IP
+cd Apps_C/P05-IP
 make
+
+#Clone the latest version of the Sensorian Interface repository into SensorianInterface
+git clone https://github.com/sensorian/SensorianInterface.git ~/SensorianInterface
+chmod +x ~/SensorianInterface/C/install.sh
+chmod +x ~/SensorianInterface/Python/install.sh
+
+#Get additional requirements for Sensorian Interface
+sudo apt-get -y install libcurl4-openssl-dev || { echo "Failed to install libcurl4-openssl-dev" && exit; }
+
+#Compile Sensorian Interface
+cd ~/SensorianInterface/C
+make
+cd ~/SensorianInterface/Python/i2c-devices-interface
+make
+cd ..
+cd PythonSharedObjectSrc
+cd MPL3115A2
+make
+cd ..
+cd FXOS8700CQR1
+make
+cd ..
+cd CAP1203
+make
+cd ..
+cd ..
+cp -p PythonSharedObjectSrc/MPL3115A2/libMPL.so ./libMPL.so
+cp -p PythonSharedObjectSrc/FXOS8700CQR1/libFXO.so ./libFXO.so
+cp -p PythonSharedObjectSrc/CAP1203/libCAP.so ./libCAP.so
+cp -p i2c-devices-interface/libsensorianplus.so ./libsensorianplus.so
+cd ..
+
+#Clone the latest version of the Sensorian Hub Client repository into SensorianHubClient
+git clone https://github.com/sensorian/SensorianHubClient.git ~/SensorianHubClient
+chmod +x ~/SensorianHubClient/install.sh
+
+#Get additional requirements for Sensorian Hub Client
+sudo pip install Flask-HTTPAuth==3.1.2
+sudo pip install Flask-RESTful==0.3.5
+
+#Compile Sensorian Hub Client
+cd ~/SensorianHubClient/PythonSharedObjectSrc
+cd MPL3115A2
+make
+cd ..
+cd FXOS8700CQR1
+make
+cd ..
+cd CAP1203
+make
+cd ..
+cd ..
+cp -p PythonSharedObjectSrc/MPL3115A2/libMPL.so ./libMPL.so
+cp -p PythonSharedObjectSrc/FXOS8700CQR1/libFXO.so ./libFXO.so
+cp -p PythonSharedObjectSrc/CAP1203/libCAP.so ./libCAP.so
+cd ..
 
 #Switch back to the build directory
 cd $builddir
